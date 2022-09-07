7d9a040c2067d7fbd6e0fbd4930f09d6669b4d0f
unofficialnikke <nicklasnystrm3@gmail.com>
2022-09-07 12:33:51 +0300

    First commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,39 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	public String title;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String author;
+	public int year;
+	public int isbn;
+	public double price;
+}
---
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package com.example.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index")
+	public String Book(Model model) {
+		return "list";
+	}
+	
+}
