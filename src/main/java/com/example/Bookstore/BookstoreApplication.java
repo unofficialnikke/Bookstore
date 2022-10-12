b6c0396a83fd5362f5485add4f024ec9fed16bfb
unofficialnikke <nicklasnystrm3@gmail.com>
2022-10-12 17:23:58 +0300

    added log in and security

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.Category;
 import com.example.Bookstore.domain.CategoryRepository;
+import com.example.Bookstore.domain.User;
+import com.example.Bookstore.domain.UserRepository;
 import com.example.Bookstore.domain.Book;
 
 @SpringBootApplication
@@ -21,7 +23,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("few categories");
 			crepository.save(new Category("Fantasy"));
@@ -32,6 +34,11 @@ public class BookstoreApplication {
 			brepository.save(new Book("J.K. Rowling", "Harry Potter",  "1999", "324253", "24.5", crepository.findByName("Fantasy").get(0)));
 			brepository.save(new Book("Kari Hotakainen", "Juoksuhaudantie", "2002", "324253", "24.00", crepository.findByName("Drama").get(0)));
 			brepository.save(new Book("J. R. R. Tolkien", "The Two Towers", "1954", "998331", "29.00", crepository.findByName("Fantasy").get(0)));
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : brepository.findAll()) {
